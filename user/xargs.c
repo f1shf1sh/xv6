26c0117d5f668cb8caede542f6ba89261dde7149
fish <1284732495@qq.com>
2023-07-24 15:40:40 +0000

    parse param have some issue

---
+++ user/xargs.c
@@ -0,0 +1,90 @@
+#include "kernel/types.h"
+#include "kernel/stat.h"
+#include "kernel/param.h"
+#include "user/user.h"
+
+int
+split(char *argv_arr[MAXARG], char *str, char delim) {
+    char *start = str;
+    char *end = str;
+    int index = 0;
+
+    if ( start == 0) {
+        return 0;
+    }
+
+    while (*end != '\0') {
+        if ( *end == delim) {
+            if (start == end) {
+                argv_arr[index] = 0;
+            } else {
+                argv_arr[index] = start;
+            }
+
+            *end = '\0';
+            start = end + 1;
+            index++;
+        } 
+        end++;
+    }
+
+    if ( start != end) {
+        argv_arr[index] = start;
+        index++;
+    }
+    // else {
+    //     index--;
+    // }
+    return index;
+    
+}
+
+int 
+main(int argc, char* argv[]) {
+
+    int i;
+    int j;
+    int pid;
+    int arrlen;
+    char argv_buf[MAXARG];
+    char *argv_arr[MAXARG];
+    // char whitespace[] = " \t\r\n\v";
+    if (argc < 2) {
+        printf("usage: xargs <parm>\n");
+        exit(0);
+    }
+
+    
+    int n = read(0,argv_buf, MAXARG);
+    if ( n < 0) {
+        fprintf(2, "xargs: read arg error\n");
+        exit(1);
+    }
+
+    // printf("arg:%s\n", argv_buf);
+    // parse argv_buf
+    arrlen = split(argv_arr, argv_buf, '\n');
+
+    //check it
+    // printf("arrlen:%d\n", arrlen);
+    // for ( i = 0; i < arrlen; i++) {
+    //     printf("%s\n", argv_arr[i]);
+    // }
+
+    for ( i = argc, j = 0; j < arrlen; i++, j++) {
+        argv[i] = argv_arr[j];
+        argc++;
+    }
+
+    for ( i = 0; i < argc; i++) {
+        printf("xargs argv[%d]:%s\n", i, argv[i]);
+    }
+
+
+    pid = fork();
+    if (pid == 0) {
+        exec(argv[1], argv);
+        exit(0);
+    } 
+    exit(0);
+}
