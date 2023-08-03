28a9d495bbb0f2f1276d3e03ee9c6fcf22cafec5
fish <1284732495@qq.com>
2023-08-03 15:09:47 +0000

    declare the protoype for sysinfo

--- kernel/syscall.c
+++ kernel/syscall.c
@@ -105,6 +105,7 @@ extern uint64 sys_wait(void);
 extern uint64 sys_write(void);
 extern uint64 sys_uptime(void);
 extern uint64 sys_trace(void);
+extern uint64 sys_sysinfo(void);
 
 static uint64 (*syscalls[])(void) = {
 [SYS_fork]    sys_fork,
@@ -129,6 +130,8 @@ static uint64 (*syscalls[])(void) = {
 [SYS_mkdir]   sys_mkdir,
 [SYS_close]   sys_close,
 [SYS_trace]   sys_trace,
+[SYS_sysinfo] sys_sysinfo,
+
 };
 
 void
@@ -139,7 +142,7 @@ syscall(void)
                                "chdir", "dup", "getpid", "sbrk", \
                                "sleep", "uptime", "open", "write", \
                                "mknod", "unlink", "link", "mkdir",  \
-                               "close", "trace"};
+                               "close", "trace", "sysinfo"};
   int num;
   struct proc *p = myproc();
   int syscallret;
